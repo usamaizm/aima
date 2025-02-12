5939d2d73f1ba17fdc4445bd39188cbf56f0fb76
Usama Ismail <usamaizm@gmail.com>
2025-02-11 22:37:37 -0800

    Update language.h

--- language.h
+++ language.h
@@ -1,1 +1,8 @@
-init
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+// Function prototypes for language processing
+void process_language_input(const char *input);
+void generate_language_output(char *output);
+
+#endif // LANGUAGE_H
