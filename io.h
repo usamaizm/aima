b979bb39b47f74493d51c7e7fa1e5791d359ef54
Usama Ismail <usamaizm@gmail.com>
2025-02-11 22:39:08 -0800

    Update io.h

--- io.h
+++ io.h
@@ -1,1 +1,8 @@
-init
+#ifndef IO_H
+#define IO_H
+
+// Function prototypes for input/output handling
+void read_input(const char *source, char *input);
+void write_output(const char *destination, const char *output);
+
+#endif // IO_H
