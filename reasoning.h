5adc36af3a7f70fa6f8c9c64fe40c5c0d885427b
Usama Ismail <usamaizm@gmail.com>
2025-02-11 22:41:06 -0800

    Create reasoning.h

---
+++ reasoning.h
@@ -0,0 +1,8 @@
+#ifndef REASONING_H
+#define REASONING_H
+
+// Function prototypes for reasoning and decision making
+void analyze_data(const char *data);
+void make_decision(char *decision);
+
+#endif // REASONING_H
