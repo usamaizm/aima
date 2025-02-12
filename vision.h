c47761107883636f83e32e94d4d0cec7e916ea19
Usama Ismail <usamaizm@gmail.com>
2025-02-11 22:38:35 -0800

    Update vision.h

--- vision.h
+++ vision.h
@@ -1,1 +1,8 @@
-init
+#ifndef VISION_H
+#define VISION_H
+
+// Function prototypes for vision processing
+void process_vision_input(const char *input);
+void generate_vision_output(char *output);
+
+#endif // VISION_H
